6659b5ff27d6b2d9d60f71e27bb74eedaab2d1a6
Daniel Borges Gonçalves <danielbgoncalves@ufu.br>
2025-10-12 18:02:23 -0300

    update: manutenção para facilitar inicialização do programa

--- codigo-fractal-python/SaveCSVPercCLACDF3Distances.py
+++ codigo-fractal-python/SaveCSVPercCLACDF3Distances.py
@@ -1,23 +1,33 @@
 import pandas as pd
 import os
 
-from ScriptLACDF3Distances import ScriptLACDF3Distances 
-from ScriptPercLACDF3Distances import ScriptPercLACDF3Distances
-
-def SaveCSVPercCLACDF3Distances(origem, destino, LAC_DF_ONLY=True):
+from ScriptLACDF3Distances import scriptLACDF3Distances 
+from ScriptPercLACDF3Distances import scriptPercLACDF3Distances
+def saveCSVPercCLACDF3Distances(origem, destino, All_FEATURES=True):
     '''
         Salva os resultados em um csv, por padrão apenas o DF e o LAC, mas podendo salvar também a percolação
     '''
 
-    if LAC_DF_ONLY: 
-        resultado = ScriptLACDF3Distances(origem)
+    if All_FEATURES: 
+        resultado = scriptPercLACDF3Distances(origem)
     else:
-        resultado = ScriptPercLACDF3Distances(origem)
+        resultado = scriptLACDF3Distances(origem) 
 
     df = pd.DataFrame(resultado)
+    os.makedirs(destino, exist_ok=True)  # garante que a pasta exista
     caminho_csv_final = os.path.join(destino, 'resultados_completos.csv')
-    df.to_csv(caminho_csv_final, index=False)
+    df.to_csv(caminho_csv_final, index=False, sep=';')
 
-    print(f"Slavo em {caminho_csv_final}")
+    print(f"Salvo em {caminho_csv_final}")
+
+if __name__ == "__main__":
+    import sys
+
+    origem = sys.argv[1]
+    destino = sys.argv[2]
+    
+    All_FEATURES = sys.argv[3].lower() == "true" if len(sys.argv) > 3 else True
+
+    saveCSVPercCLACDF3Distances(origem, destino)
 
 
