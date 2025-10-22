252543b86a29bfb9a68302c2d3ad648d0209445e
Daniel Borges Gonçalves <danielbgoncalves@ufu.br>
2025-10-21 22:56:49 -0300

    fix: chamada de funções consertada

--- codigo-fractal-python/SaveCSVPercCLACDF3Distances.py
+++ codigo-fractal-python/SaveCSVPercCLACDF3Distances.py
@@ -7,14 +7,19 @@ def saveCSVPercCLACDF3Distances(origem, destino, All_FEATURES=True):
     '''
         Salva os resultados em um csv, por padrão apenas o DF e o LAC, mas podendo salvar também a percolação
     '''
-
+    resultado = []
     if All_FEATURES: 
-        resultado = scriptPercLACDF3Distances(origem)
+        print('Calculando perc, LAC e DF')
+        resultado_perc = scriptPercLACDF3Distances(origem)
+        resultado_LACDF = scriptLACDF3Distances(origem) 
+        
+        resultado = resultado_perc + resultado_LACDF  
     else:
+        print('calculando LAC e DF')
         resultado = scriptLACDF3Distances(origem) 
 
     df = pd.DataFrame(resultado)
-    os.makedirs(destino, exist_ok=True)  # garante que a pasta exista
+    os.makedirs(destino, exist_ok=True)  
     caminho_csv_final = os.path.join(destino, 'resultados_completos.csv')
     df.to_csv(caminho_csv_final, index=False, sep=';')
 
@@ -26,8 +31,8 @@ if __name__ == "__main__":
     origem = sys.argv[1]
     destino = sys.argv[2]
     
-    All_FEATURES = sys.argv[3].lower() == "true" if len(sys.argv) > 3 else True
+    All_FEATURES = sys.argv[3].lower() == "true" 
 
-    saveCSVPercCLACDF3Distances(origem, destino)
+    saveCSVPercCLACDF3Distances(origem, destino, All_FEATURES)
 
 
