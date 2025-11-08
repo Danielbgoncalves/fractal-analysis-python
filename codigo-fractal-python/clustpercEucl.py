fc15b494601f78d703868fc76b7888d9d29a4d04
Daniel Borges Gonçalves <danielbgoncalves@ufu.br>
2025-11-08 11:11:42 -0300

    feature: adiciona versão das funções clustperc com numba

--- codigo-fractal-python/clustpercEucl.py
+++ codigo-fractal-python/clustpercEucl.py
@@ -2,7 +2,181 @@ import numpy as np
 from joblib import Parallel, delayed
 from scipy.ndimage import label
 from scipy.stats import skew
+from numba import njit
 
+@njit
+def _label_4conn(box):
+    """
+    Retorna num_features (int), tamanho_maior_cluster (int)
+    Assim como label do scikit fazia, mas como o numba não converte 
+    dessa biblioteca tem q implemnetar NA MÃO
+    """
+    h, w = box.shape
+    labels = -1 * np.ones((h, w), dtype=np.int32)
+    stack_i = np.empty(h*w, dtype=np.int32)
+    stack_j = np.empty(h*w, dtype=np.int32)
+    cur_label = 0
+    max_cluster = 0
+
+    for i in range(h):
+        for j in range(w):
+            if box[i, j] != 0 and labels[i, j] == -1:
+
+                stack_ptr = 0
+                stack_i[stack_ptr] = i
+                stack_j[stack_ptr] = j
+                stack_ptr += 1
+                labels[i, j] = cur_label
+                comp_size = 0
+
+                while stack_ptr > 0:
+                    stack_ptr -= 1
+                    ci = stack_i[stack_ptr]
+                    cj = stack_j[stack_ptr]
+                    comp_size += 1
+
+                    # vizinhos 4 conexos!
+                    if ci - 1 >= 0 and box[ci-1, cj] != 0 and labels[ci-1, cj] == -1:
+                        labels[ci-1, cj] = cur_label
+                        stack_i[stack_ptr] = ci-1
+                        stack_j[stack_ptr] = cj
+                        stack_ptr += 1
+                    if ci + 1 < h and box[ci+1, cj] != 0 and labels[ci+1, cj] == -1:
+                        labels[ci+1, cj] = cur_label
+                        stack_i[stack_ptr] = ci+1
+                        stack_j[stack_ptr] = cj
+                        stack_ptr += 1
+                    if cj - 1 >= 0 and box[ci, cj-1] != 0 and labels[ci, cj-1] == -1:
+                        labels[ci, cj-1] = cur_label
+                        stack_i[stack_ptr] = ci
+                        stack_j[stack_ptr] = cj-1
+                        stack_ptr += 1
+                    if cj + 1 < w and box[ci, cj+1] != 0 and labels[ci, cj+1] == -1:
+                        labels[ci, cj+1] = cur_label
+                        stack_i[stack_ptr] = ci
+                        stack_j[stack_ptr] = cj+1
+                        stack_ptr += 1
+
+                if comp_size > max_cluster:
+                    max_cluster = comp_size
+                cur_label += 1
+
+    return cur_label, max_cluster
+
+@njit
+def analisar_um_raio_jit(k, r_k, img_aux):
+    """
+    Versão em Numba da função que analisa um raio r_k.
+    Retorna (k, p_k, g_k, h_k)
+    """
+    h_img, w_img, _ = img_aux.shape
+    ncaixas = (h_img - r_k + 1) * (w_img - r_k + 1)
+    if ncaixas <= 0:
+        return k, 0.0, 0.0, 0.0
+
+    vetBigClusteres_sum = 0.0  
+    ptemp = 0.0
+    gtemp = 0.0
+    lim = (r_k / 2.0) - 0.5
+
+    caixa_idx = 0
+    for x in range(int(lim), int(h_img - lim)):
+        for y in range(int(lim), int(w_img - lim)):
+            xi = int(x - lim)
+            xf = int(x + lim)
+            yi = int(y - lim)
+            yf = int(y + lim)
+
+            rows = xf - xi + 1
+            cols = yf - yi + 1
+            # criar box local
+            box = np.zeros((rows, cols), dtype=np.uint8)
+
+            percCount = 0
+            a = -1
+            for i in range(xi, xf + 1):
+                a += 1
+                b = -1 
+                for j in range(yi, yf + 1):
+                    b += 1
+                    dist = np.sqrt(
+                        ((img_aux[i, j, 0] - img_aux[x, y, 0]) ** 2 ) +  \
+                        ((img_aux[i, j, 1] - img_aux[x, y, 1]) ** 2 ) + \
+                        ((img_aux[i, j, 2] - img_aux[x, y, 2]) ** 2 )
+                    )
+                    if dist <= r_k:
+                        box[a,b] = 1
+                        percCount += 1
+                    else:
+                        box[a, b] = 0
+
+            num_features, tamanho_maior = _label_4conn(box)
+
+            vetBigClusteres_sum += (tamanho_maior / (r_k * r_k))
+            ptemp += num_features
+
+            if (percCount / (r_k * r_k)) >= 0.59275:
+                gtemp += 1
+
+            caixa_idx += 1
+
+    p_k = ptemp / ncaixas
+    g_k = gtemp / ncaixas
+    h_k = vetBigClusteres_sum / ncaixas
+
+    return k, p_k, g_k, h_k
+
+def clustpercEucl_jit(img, maxr):
+    aux = img.astype(np.float64)
+    r = list(range(3, maxr + 1, 2))
+
+    g = np.zeros(len(r))
+    p = np.zeros(len(r))
+    h = np.zeros(len(r))
+
+    for k in range(len(r)):
+        _, p_k, g_k, h_k = analisar_um_raio_jit(k, r[k], aux)
+        p[k] = p_k
+        g[k] = g_k
+        h[k] = h_k
+
+
+    AreaCluster = np.trapz(p) # área sobre a curva, uma integral
+    AreaPerc = np.trapz(g)
+    AreaMaxCluster = np.trapz(h)
+    SkewnessCluster = skew(p) # o quão assimétrica é a curva
+    SkewnessPerc = skew(g)
+    SkewnessMaxCluster = skew(h)
+    [MaxCluster,MaxClusterIndex] = np.max(p), np.argmax(p) # o maior valor e seu index (da curva)
+    [MaxPerc,MaxPercIndex] = np.max(g), np.argmax(g)
+    [MaxMaxCluster,MaxMaxClusterIndex] = np.max(h), np.argmax(h)
+    half = int(np.ceil(len(p)/2))
+    AreaRatioCluster = np.trapz(p[half:])/np.trapz(p[:half])
+    AreaRatioPerc = np.trapz(g[half:])/np.trapz(g[:half])
+    AreaRatioMaxCluster = np.trapz(h[half:])/np.trapz(h[:half])
+
+    return {
+        'EuclMaxClusterIndex': MaxClusterIndex,
+        'EuclMaxPercIndex': MaxPercIndex,
+        'EuclMaxMaxClusterIndex': MaxMaxClusterIndex,
+        'EuclAreaRatioMaxCluster': AreaRatioMaxCluster,
+        'EuclMaxMaxCluster': MaxMaxCluster,
+        'EuclSkewnessMaxCluster': SkewnessMaxCluster,
+        'EuclAreaMaxCluster': AreaMaxCluster,
+        'EuclAreaRatioCluster': AreaRatioCluster,
+        'EuclAreaRatioPerc': AreaRatioPerc,
+        'EuclMaxCluster': MaxCluster,
+        'EuclMaxPerc': MaxPerc,
+        'EuclSkewnessCluster': SkewnessCluster,
+        'EuclSkewnessPerc': SkewnessPerc,
+        'EuclAreaPerc': AreaPerc,
+        'EuclAreaCluster': AreaCluster,
+        'Euclp': p,
+        'Euclg': g,
+        'Euclh': h
+    }
+
+# ==== Sem NUMBA ====
 
 def analisar_um_raio(k, r_k, img_aux):
     '''
@@ -70,7 +244,6 @@ def analisar_um_raio(k, r_k, img_aux):
 
     return k, p_k, g_k, h_k
 
-
 def clustpercEucl(img, maxr):
     aux = img.astype(np.float64)
     r = list(range(3, maxr + 1, 2)) # [3,5,7,...maxr]
@@ -88,7 +261,7 @@ def clustpercEucl(img, maxr):
         p[int(k)] = p_k
         g[int(k)] = g_k
         h[int(k)] = h_k
-
+    
 
     AreaCluster = np.trapz(p) # área sobre a curva, uma integral
     AreaPerc = np.trapz(g)
